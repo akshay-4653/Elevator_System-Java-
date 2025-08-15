764511ed6f71bd20cdb3d09c0c64b5cbf2d49f37
akshaykumar-4653 <akshaykumarboda4653@gmail.com>
2025-08-15 14:14:56 +0530

    Elevator_project

---
+++ 2.Direction.java
@@ -0,0 +1,13 @@
+package elevator;
+
+/*
+ * Direction in which the elevator can move.
+ * UP    – elevator moves to a higher floor.
+ * DOWN  – elevator moves to a lower floor.
+ * IDLE  – elevator is stationary, awaiting new requests.
+ */
+public enum Direction {
+    UP,
+    DOWN,
+    IDLE
+}
